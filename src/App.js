4777e462502b240f47516de3d55552966c1bb5a4
Zeztron <pateltharsh@gmail.com>
2019-02-07 09:19:18 -0500

    added title bar

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table className="title-bar">
+          <tbody>
+            <tr>
+              <td>
+                <img width="50" src="logo.svg" alt="Movie DB"/>
+              </td>
+              <td style={{
+                paddingLeft: 16
+              }}>
+                <h1>
+                  MoviesDB Search
+                </h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
